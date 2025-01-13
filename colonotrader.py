e1ed408b201d6462c2a1eeb24ae234fb2c04d14e
fabiolrodriguez <fabio.rodriguez@gmail.com>
2025-01-13 07:21:13 -0300

    Add state file to save positioned value

--- colonotrader.py
+++ colonotrader.py
@@ -5,12 +5,34 @@ import os
 import time
 import asyncio
 import logging
+import json
+
 
 # Load the .env file
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
+
+## Save the position on json file
+STATE_FILE = "state.json"
+
+def save_state(positioned):
+    """Save the positioned value to a JSON file."""
+    state = {"positioned": positioned}
+    with open(STATE_FILE, "w") as file:
+        json.dump(state, file)
+
+def load_state():
+    """Load the positioned value from a JSON file."""
+    try:
+        with open(STATE_FILE, "r") as file:
+            state = json.load(file)
+            return state.get("positioned", False)  # Default to False if key doesn't exist
+    except FileNotFoundError:
+        return False  # Default if file doesn't exist
+###
+        
 
 client = Client(API_KEY,API_SECRET,testnet=True)
 
@@ -46,25 +68,28 @@ async def place_sell_order(symbol,quantity):
 
 async def trading_bot():
     
-    positioned = True
+    positioned = load_state()  # Load the state from the JSON file
+    print(f"Loaded positioned: {positioned}")
+
     currency = await get_current_price(symbol)
     
     if currency is None:
         await asyncio.sleep(5)  # Retry after 5 seconds if there was an error
         
-    print(f" {symbol} current price is {currency}")
+    print(f"{symbol} current price is {currency}")
 
     if positioned:
         if currency > sell_price:
             print(f"Price is {currency} placing sell order")
             await place_sell_order(symbol,trade_quantity)
             positioned = False
+            save_state(positioned)            
     else:
         if currency < buy_price:
             print(f"Price is {currency} placing buy order")
             await place_buy_order(symbol,trade_quantity)
             positioned = True
-
+            save_state(positioned)
 
 async def main():
 
